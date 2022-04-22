69afaf05713e3280c18a2946344ca9a8993e2c3b
Solution For Client <s2032193@students.ncl-coll.ac.uk>
2022-04-22 16:03:43 +0100

    Full push
    123

---
+++ jsmain.js
@@ -0,0 +1,16 @@
+function setNewImage()
+{
+    document.getElementById("H1").src ="images/5out5.png";
+    document.getElementById("H2").src ="images/4out5.png";
+    document.getElementById("H3").src ="images/5out5.png";
+    document.getElementById("S1").src ="images/1out5.png";
+    document.getElementById("S2").src ="images/1out5.png";
+}
+function setOldImage()
+{
+    document.getElementById("H1").src ="images/happyF.png";
+    document.getElementById("H2").src ="images/happyF.png";
+    document.getElementById("H3").src ="images/happyF.png";
+    document.getElementById("S1").src ="images/sadF.png";
+    document.getElementById("S2").src ="images/sadF.png";
+}
